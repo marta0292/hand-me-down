51b46c81ac5354d72c0de4b5db23376f8637be32
Marta Kulesza <mar.kulesza.92@gmail.com>
2020-03-23 23:10:45 +0100

    section About Us almost ready

--- src/components/Home/HomeAboutUs.jsx
+++ src/components/Home/HomeAboutUs.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import "../../scss/components/home/_homeAboutUs.scss"
+import "../../scss/components/home/_homeAboutUs.scss";
+import Decoration from '../../assets/Decoration.svg';
+import Signature from '../../assets/Signature.svg';
 
 const HomeAboutUs = () => {
     return (
         <div id="about" className='home-about-us'>
-            AboutUs
+            <div className='home-about-us-content'>
+                <p className='about-us-title'>O nas</p>
+                <img src={Decoration} alt='decoration'/>
+                <p className='about-us-text'>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <div className='about-us-signature'>
+                    <img src={Signature}
+                         alt='signature'/>
+                </div>
+            </div>
+            <div className='home-about-us-picture'></div>
         </div>
     );
 };
